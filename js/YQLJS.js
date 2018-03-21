c2afd0787e2fadd316c0e609deac9b0109574ead
Tim C <timoman8@hotmail.com>
2018-03-21 19:49:28 -0400

    Upload to github

---
+++ js/YQLJS.js
@@ -0,0 +1,136 @@
+const YQLJS = {
+  info:{
+    author: 'Timothy C. (SSJ3 Bane / timoman6,7,8)',
+    repo: 'https://timoman7.github.io/'
+  }
+};
+function checkType(query){
+  let types={
+    html:[
+      'htmlstring',
+      'html'
+    ],
+    xml:[
+      'xmlstring',
+      'xml'
+    ],
+    json:[
+      'json',
+      'steam.user'
+    ],
+    text:[
+      'string'
+    ]
+  };
+  let thisType = 'text';
+  Object.keys(types).forEach((kn)=>{
+    let type = types[kn];
+    type.forEach((dataType)=>{
+      let regMatch = query.match(new RegExp(`from ${dataType} where`,'i'));
+      if(regMatch != null){
+        thisType = kn;
+      }
+    });
+  })
+  return thisType;
+}
+function bossify(str){
+  let escapeValues = {
+    '%': '25',
+    '/': '2F',
+    '?': '3F',
+    '&': '26',
+    ';': '3B',
+    ':': '3A',
+    '@': '40',
+    ',': '2C',
+    '$': '24',
+    '=': '3D',
+    ' ': '20',
+    '"': '22',
+    '+': '2B',
+    '#': '23',
+    '*': '2A',
+    '<': '3C',
+    '>': '3E',
+    '{': '7B',
+    '}': '7D',
+    '|': '7C',
+    '[': '5B',
+    ']': '5D',
+    '^': '5E',
+    '\\': '5C',
+    '`': '60',
+    '(': '28',
+    ')': '29'
+  };
+  let newStr="";
+  newStr=str;
+  for(let evKey in escapeValues){
+    let ev = escapeValues[evKey];
+    newStr=newStr.replace(new RegExp('\\'+evKey,'g'),'%'+ev);
+  }
+  return newStr;
+}
+let YQLAjax = (function(_ajax){
+  var protocol = location.protocol,
+    hostname = location.hostname,
+    exRegex = RegExp(protocol + '//' + hostname),
+    YQLURL = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?callback=?',
+    query = `select * from html where url="{URL}" and xpath="*"`;
+
+  function isExternal(url)
+  {
+    return !exRegex.test(url) && /:\/\//.test(url);
+  }
+
+  return function(query,options){
+    let o = {};
+    // Manipulate options so that JSONP-x request is made to YQL
+    o.url = YQLURL;
+    o.dataType = 'json';
+    o.data = Object.assign({
+      q: query,
+      format: 'json',
+      diagnostics: true,
+      env:'store://datatables.org/alltableswithkeys'
+    },options);
+
+    // Since it's a JSONP request
+    // complete === success
+    if (!o.success && o.complete) {
+      o.success = o.complete;
+      delete o.complete;
+    }
+
+    o.success = (function(_success){
+      return function(data){
+        if (_success) {
+          // Fake XHR callback.
+          _success.call(this, {
+            // YQL screws with <script>s, Get rid of them
+            responseText: data
+          }, 'success');
+        }
+      };
+    })(o.success);
+    return $.ajax(o); // not special, use base Jquery ajax
+  };
+})(jQuery.ajax);
+function YQLQuery(query, params, options){
+  let _YQLQuery = '';
+  _YQLQuery = query;
+  for(let kn in params){
+    let kv = params[kn];
+    _YQLQuery += ` ${kn}='${kv}'${(Object.keys(params)[Object.keys(params).length-1]!=kn?' AND':'')}`;
+  }
+  let returnAjax = YQLAjax(_YQLQuery, ((options!=undefined&&options instanceof Object&&!(options instanceof Array))?options:{}));
+  return returnAjax;
+}
+async function YQL(query, params){
+  console.log(query, params)
+  let response = await YQLQuery(query, params);
+  let responseData = response.query;
+  let responseType = checkType(query);
+  return [responseData, responseType];
+}
---
+++ js/main.js
@@ -0,0 +1,52 @@
+function removeParam(e){
+  e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode);
+}
+//function main(){
+  let paramTemplate = document.getElementById('param');
+  let inputHolder = document.getElementById('inputHolder');
+  let queryInput = document.getElementById('query');
+  let paramsHolder = document.getElementById('params');
+  let addParamBtn = document.getElementById('addParamBtn');
+  let submitBtn = document.getElementById('submitBtn');
+  let debugBtn = document.getElementById('debugBtn');
+  let output = document.getElementById('output');
+
+  function addParam(){
+    let newParam = document.importNode(paramTemplate.content, true);
+    paramsHolder.appendChild(newParam);
+  }
+  function parseParams(el){
+    console.log(el)
+    let allParams = {};
+    return allParams;
+  }
+  function responseStringify(data, mode){
+    if(mode == 'normal'){
+      return JSON.stringify(data.results, undefined, 2);
+    }else if(mode == 'debug'){
+      return JSON.stringify(data.diagnostics, undefined, 2);
+    }else{
+      return JSON.stringify({
+        'Error':`No mode specified. Contact ${YQLJS.info.author} at ${YQLJS.info.repo}`
+      }, undefined, 2);
+    }
+  }
+  function displayOutput(mode, _header, data, format, _footer){
+    console.log(data,format)
+    if(format == 'json'){
+      output.innerHTML = `${_header}<br>${responseStringify(data, mode)}<br>${format}${_footer!=undefined?'<br>'+_footer:''}`;
+    }else if(format == 'html'){
+      output.innerHTML = `${_header}<br>${data.results.result}<br>${format}${_footer!=undefined?'<br>'+_footer:''}`;
+    }
+  }
+  addParamBtn.addEventListener('click', function(){
+    addParam();
+  });
+  submitBtn.addEventListener('click', async function(){
+    displayOutput('normal', '<h1>Output</h1>', ... await YQL(queryInput.value, parseParams(paramsHolder)));
+  });
+  debugBtn.addEventListener('click', async function(){
+    displayOutput('debug', '<h1>Diagnostics</h1><pre>', ... await YQL(queryInput.value, parseParams(paramsHolder)),'</pre>');
+  });
+//}
+//document.addEventListener('load',main);
